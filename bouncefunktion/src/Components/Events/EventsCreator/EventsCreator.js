ab9d65f5ebbf458cca4da5d64d6fb9317dd762a2
Jim Berry <62150078+Jamboree97@users.noreply.github.com>
2020-11-04 22:42:52 +0000

    Starting the events section

--- bouncefunktion/src/Components/Events/EventsCreator/EventsCreator.js
+++ bouncefunktion/src/Components/Events/EventsCreator/EventsCreator.js
@@ -1,9 +1,27 @@
-import classes from './EventsCreator.module.css';
+import './EventsCreator.css';
 
 const EventsCreator = () => {
+  // Type of event, e.g. club, festival or live music
+  // Genre of music
+  // Date of event
+  // Start and end times of the event
+  // Ticket price
+  // Ticket purchase link
+  // Location of event
+  // Name of venue
+  // Title of post
+  // Description of event
+  // Host of event
   return (
-    <div>
-      <p style={{ color: 'black' }}>This is the events creator component</p>
+    <div className="eventsCreator__container">
+      <div className="eventsCreator__title">
+        <h2>This is the events creator component</h2>
+      </div>
+      <p>
+        Soon you'll be able to input details about a festival, night out or live
+        music event and it will display for others to register their interest
+        and buy tickets!
+      </p>
     </div>
   );
 };
--- bouncefunktion/src/Containers/EventsContainer/EventsContainer.js
+++ bouncefunktion/src/Containers/EventsContainer/EventsContainer.js
@@ -1,9 +1,17 @@
 import './EventsContainer.css';
+import { useState, useEffect } from 'react';
 import EventsCreator from '../../Components/Events/EventsCreator/EventsCreator';
+import BlueButton from '../../UI/Modal/Buttons/BlueButton/BlueButton';
 
 const EventsContainer = (props) => {
   // Event container should contain lots of information, I basically want a longish form for someone to fill out which details all the information you'd need for a night out
 
+  const [showEventsCreator, setShowEventsCreator] = useState(false);
+
+  const testMethod = () => {
+    console.log('Button pressed');
+    setShowEventsCreator((oldValue) => !oldValue);
+  };
   // Type of event, e.g. club, festival or live music
   // Genre of music
   // Date of event
@@ -17,8 +25,10 @@ const EventsContainer = (props) => {
   // Host of event
   return (
     <div>
-      <p>Events information will go here</p>
-      <EventsCreator />
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <BlueButton clicked={testMethod}>Create an Event</BlueButton>
+      </div>
+      {showEventsCreator ? <EventsCreator /> : null}
     </div>
   );
 };
