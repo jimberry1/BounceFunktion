ca5a1e3e3972e8658d6dd234cc3a1f7d749f0ede
Jim Berry <jimalexanderberry@gmail.com>
2020-11-11 20:39:08 +0000

    Some weird styling that was broken on windows and not on my mac

--- bouncefunktion/src/Components/Post/PostComments/PostComment/PostComment.js
+++ bouncefunktion/src/Components/Post/PostComments/PostComment/PostComment.js
@@ -2,9 +2,12 @@ import './PostComment.css';
 import { Avatar } from '@material-ui/core';
 const PostComment = (props) => {
   return (
-    <div className="postComment__holder">
-      <Avatar src={props.profilePic} />
-      <p className={'postComment__name'}>
+    <div
+      className="postComment__name"
+      style={{ borderTop: '1px solid lightgray' }}
+    >
+      <Avatar src={props.profilePic} style={{ alignContent: 'center' }} />
+      <p className="postComment__name">
         <strong>{props.poster}</strong>
       </p>
       <p className="postComment__comment">{props.message}</p>
