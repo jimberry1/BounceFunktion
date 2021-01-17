e009e7dbfc92c2a86549a058aa490246d542d579
Jim Berry <jimalexanderberry@gmail.com>
2021-01-17 16:00:45 +0000

    PWA changes

--- bouncefunktion/src/index.js
+++ bouncefunktion/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import reducer, { initialState } from './Store/reducer';
@@ -23,3 +24,6 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+serviceWorker.register();
