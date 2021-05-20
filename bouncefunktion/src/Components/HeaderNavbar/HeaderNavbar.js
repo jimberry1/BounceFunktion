af0c252804b443871aa5a97201b3b5b93b3e5311
Jim Berry <jimalexanderberry@gmail.com>
2021-05-20 18:49:56 +0100

    adding no auth redirects

--- bouncefunktion/src/Components/HeaderNavbar/HeaderNavbar.js
+++ bouncefunktion/src/Components/HeaderNavbar/HeaderNavbar.js
@@ -2,8 +2,11 @@ import './HeaderNavbar.css';
 import { Nav, Navbar } from 'react-bootstrap';
 import Toggler from '../Theme/Toggler';
 import { Link } from 'react-router-dom';
+import { useStateValue } from '../../Store/StateProvider';
 
 const HeaderNavbar = (props) => {
+  const [{ user }] = useStateValue();
+  console.log(user);
   return (
     <div>
       <Navbar bg="dark" variant="dark">
@@ -11,22 +14,34 @@ const HeaderNavbar = (props) => {
           BounceFunktion
         </Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link as={Link} to="/feed">
+          <Nav.Link as={Link} to={user ? '/feed' : '/signin?redirectTo=feed'}>
             Feed
           </Nav.Link>
-          <Nav.Link as={Link} to="/mixes">
+          <Nav.Link as={Link} to={user ? '/mixes' : '/signin?redirectTo=mixes'}>
             Mixes
           </Nav.Link>
-          <Nav.Link as={Link} to="/events">
+          <Nav.Link
+            as={Link}
+            to={user ? '/events' : '/signin?redirectTo=events'}
+          >
             Events
           </Nav.Link>
-          <Nav.Link as={Link} to="/community">
+          <Nav.Link
+            as={Link}
+            to={user ? '/community' : '/signin?redirectTo=community'}
+          >
             Members
           </Nav.Link>
-          <Nav.Link as={Link} to="/profile">
+          <Nav.Link
+            as={Link}
+            to={user ? '/profile' : '/signin?redirectTo=profile'}
+          >
             Profile
           </Nav.Link>
-          <Nav.Link as={Link} to="/feedback">
+          <Nav.Link
+            as={Link}
+            to={user ? '/feedback' : '/signin?redirectTo=feedback'}
+          >
             Feedback
           </Nav.Link>
         </Nav>
